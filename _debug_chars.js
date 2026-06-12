c0357e6b7d69eddf0f7babaa245a2d3d5fea29d2
Ersin KOÇ <ersinkoc@gmail.com>
2026-06-12 10:30:42 +0300

    feat: update code

--- _debug_chars.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const c = fs.readFileSync('packages/core/src/sdd/task-visualizer.ts', 'utf8');
-const lines = c.split('\n');
-for (let i = 228; i < 236; i++) {
-  console.log(i + 1, JSON.stringify(lines[i]));
-}
