1fa8d677601f314d2c9d9732c40f01cca7d1ebae
Ersin KOÇ <ersinkoc@gmail.com>
2026-06-30 19:23:20 +0300

    chore(version): bump to 0.277.2

--- website/src/lib/utils.ts
+++ website/src/lib/utils.ts
@@ -11,7 +11,7 @@ export function cn(...inputs: ClassValue[]) {
    ========================================================================= */
 
 export const META = {
-  version: '0.277.1',
+  version: '0.277.2',
   repo: 'https://github.com/WrongStack/WrongStack',
   npm: 'wrongstack',
   node: '22',
